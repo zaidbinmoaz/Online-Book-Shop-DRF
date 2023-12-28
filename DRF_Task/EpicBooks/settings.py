e4ef4bca8370c8431d8aaa730a54abb61ff9b147
zaidbinmoaz <zaidbinmoaz@gmail.com>
2023-12-28 19:31:24 +0500

    postgres connected

--- DRF_Task/EpicBooks/settings.py
+++ DRF_Task/EpicBooks/settings.py
@@ -77,12 +77,33 @@ WSGI_APPLICATION = 'EpicBooks.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "OPTIONS": {
+#             "service": "my_service",
+#             "passfile": ".my_pgpass",
+#         },
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'db_books',                      
+        'USER': 'zaidm',
+        'PASSWORD': 'admin',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
